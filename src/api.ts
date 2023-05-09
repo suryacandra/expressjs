5cf6fdfab31a4631e1641ae80aa6eee172e05249
suryacandra <scda404@gmail.com>
2023-05-09 09:08:58 +0000

    update api

--- src/api.ts
+++ src/api.ts
@@ -15,7 +15,7 @@ const bucketName = 'ripepno';
 
 const downloadParams = {
     Key: "1683612738591-StarRail 2023-05-02 09-03-09-894.mp4",
-    Bucket: bucketName
+    Bucket: bucketName,
 }
 
 export const app = express();
@@ -39,8 +39,42 @@ api.get('/hello', (req, res) => {
 
 api.get('/file', (req, res) => {
     const readableObject = s3.getObject(downloadParams).createReadStream()
+    console.log(readableObject)
     res.setHeader('Content-Disposition', `attachment; filename=1683612738591-StarRail 2023-05-02 09-03-09-894.mp4`);
     readableObject.pipe(res)
+})
+
+api.get('/video', (req, res) => {
+    const range = req.headers.range;
+    if (!range) {
+        s3.headObject(downloadParams, (err, data) => {
+            const headers = {
+                'Content-Disposition': `attachment; filename=1683612738591-StarRail 2023-05-02 09-03-09-894.mp4`,
+                "Content-Length": data.ContentLength,
+                "Content-Type": "video/mp4",
+            };
+            res.writeHead(200, headers);
+            s3.getObject(downloadParams).createReadStream().pipe(res);
+            return
+        })
+    } else {
+        s3.headObject(downloadParams, (err, data) => {
+            console.log(data)
+            const videoSize = data.ContentLength
+            const CHUNK_SIZE = 10 ** 6;
+            const start = Number(range.replace(/\D/g, ""));
+            const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+            const contentLength = end - start + 1;
+            const headers = {
+                "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+                "Accept-Ranges": "bytes",
+                "Content-Length": contentLength,
+                "Content-Type": "video/mp4",
+            };
+            res.writeHead(206, headers);
+            s3.getObject({...downloadParams, Range: range}).createReadStream().pipe(res);
+        })
+    }
 })
 
 // Version the api
